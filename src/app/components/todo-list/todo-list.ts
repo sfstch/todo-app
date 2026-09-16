0a6de39b7279dd1ee29c205534199af414ead23d
Sofia Sioutch <sofiasioutch@yandex.ru>
2026-09-17 02:12:16 +0300

    Add todo-list component with styles and tests

---
+++ src/app/components/todo-list/todo-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-todo-list',
+  styleUrl: './todo-list.css',
+  templateUrl: './todo-list.html',
+})
+export class TodoList {}
